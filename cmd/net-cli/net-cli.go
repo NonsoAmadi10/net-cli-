941977329a93582ae7b78aeaf802adbb5a6161aa
Houdini <nonsoamadi@aol.com>
2022-01-24 12:20:38 +0100

    :hammer: add cli usage

--- cmd/net-cli/net-cli.go
+++ cmd/net-cli/net-cli.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	
+	"github.com/urfave/cli"
+)
 
 func main(){
 	fmt.Println("net-cli v0.0.10")
+
+	err := cli.NewApp().Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
